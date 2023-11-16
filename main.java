e9f1ca5c3084e7c148597fd4439f880fc4829b4e
Joven Rosales <104308582+JayVen02@users.noreply.github.com>
2023-11-16 09:06:47 +0800

    Update main.java

--- main.java
+++ main.java
@@ -1,9 +1,30 @@
 import java.util.Scanner;
 
+class Task{
+      String description;
+      int priority;
+      String dueDate;
+      boolean isComplete;
+
+      Task(String description, int priority, String dueDate){
+        this.description = description;
+        this.priority = priority;
+        this.priority = priority;
+        this.dueDate = dueDate;
+        this.isComplete = false;
+      }
+}
+
+//Method of addTask, TaskList, markTaskComplete, and displayTask
+class taskList{
+
+}
+
 public class main{
     public static void main(String[] args){
         Scanner option = new Scanner(System.in);
 
+        //Option
         while(true){
             System.out.println("1. Add Task");
             System.out.println("2. Display Tasks");
@@ -16,11 +37,33 @@ public class main{
 
             switch (choice) {
                 case 1:
-                    
+                    System.out.print("Enter Task Description");
+                    String description = option.nextLine();
+                    System.out.print("Enter Priority (1-5):");
+                    int priority = option.nextInt();
+                    option.nextLine();
+                    System.out.print("Enter Due date");
+                    String dueDate = option.nextLine();
+
+                    Task newTask = new Task(description, priority, dueDate);
+                    taskList.addTask(newTask);
+                    System.out.println("Task added Succesfully");
                     break;
-            
+                case 2:
+                    System.out.println("Task List:");
+                    taskList.displayTasks();
+                    break;
+                case 3:
+                    System.out.print("Enter Task Description to mark as complete: ");
+                    String taskDescription = scanner.nextLine();
+                    taskList.markTaskComplete(taskDescription);
+                    System.out.println("Task marked as complete!");
+                    break;
+                case 4:
+                    System.out.println("Exiting program. Goodbye!");
+                    System.exit(0);
                 default:
-                    break;
+                    System.out.println("Invalid choice. Please choose a valid option.");
             }
         }
     }
